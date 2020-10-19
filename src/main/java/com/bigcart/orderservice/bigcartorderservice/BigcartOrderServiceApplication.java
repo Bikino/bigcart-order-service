95a97e82a01b13188ebc5c7b02b21d0733c63ecf
Ildephonse BIKINO <ildephonsebikino@Ildephonses-MacBook-Pro.local>
2020-10-19 09:07:55 -0400

    First commit

---
+++ src/main/java/com/bigcart/orderservice/bigcartorderservice/BigcartOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigcart.orderservice.bigcartorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcartOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigcartOrderServiceApplication.class, args);
+    }
+
+}
